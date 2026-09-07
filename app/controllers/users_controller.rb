9973fdbb4695d4d6c4f8654884f4789f390b819e
xmasuhai <xmasuhai@163.com>
2026-09-07 15:58:23 +0000

    添加用户邮箱字段验证；更新用户控制器以返回 JSON 响应

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,18 @@ class UsersController < ApplicationController
   def create
     user = User.new email: "frank@x.com", name: "frank"
     if user.save
-      p 'save 成功了' # rubocop:disable Style/StringLiterals
+      render json: user
     else
-      p 'save 失败了' # rubocop:disable Style/StringLiterals
+      render json: user.errors
     end
   end
 
   def show
-    p "你访问了 show"
+    user = User.find_by_id params[:id]
+    if user
+      render json: user
+    else
+      head 404
+    end
   end
 end
