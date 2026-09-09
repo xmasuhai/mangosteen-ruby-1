1ee7e34c18cd91d9f5393eeb1e2bb93fca12f1e2
xmasuhai <24904259+xmasuhai@users.noreply.github.com>
2026-09-09 15:17:31 +0000

    重构 API 路由结构；删除用户控制器

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  post "/users", to: "users#create"
-  get "/users/:id", to: "users#show"
+  namespace :api do
+    namespace :v1 do
+      # /api/v1
+      resources :validation_codes, only: [:create]
+      resources :session, only: [:create, :destroy]
+      resources :me, only: [:show]
+      resources :items, exclude: [:put]
+      resources :tags, exclude: [:put]
+    end
+  end
+
 end
