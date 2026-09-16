0a9d5a7be77dcc384a585e9a91186b24a4fe4b3c
xmasuhai <24904259+xmasuhai@users.noreply.github.com>
2026-09-16 13:29:56 +0000

    config(database): 更新数据库配置和部署脚本
    - 修正测试环境数据库名称从 mangosteen_1_test 到 mangosteen_test
    - 更新生产环境数据库配置，统一用户名为 mangosteen
    - 生产环境数据库密码变量从 MANGOSTEEN_1_DATABASE_PASSWORD 改为 DB_PASSWORD
    - 添加生产环境数据库主机配置支持
    - 部署脚本中添加 DB_HOST 环境变量传递
    - 部署脚本中增加数据库哈希获取提示信息

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -12,7 +12,8 @@ if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
   docker rm -f $container_name
 fi
 echo 'docker run ...'
-docker run -d -p 3000:3000 --network=network1 -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
+docker run -d -p 3000:3000 --network=network1 -e DB_HOST=$DB_HOST -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
 echo 'docker exec db...'
+echo 'get DB HASH is...'
 docker exec -it $container_name bin/rails db:create db:migrate
 echo 'DONE!'
